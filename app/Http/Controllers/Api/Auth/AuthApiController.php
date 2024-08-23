4ed500d76defe6fc6a60e83c845aa73de0050b46
Wilian Marroquin <129982946+Will-marro@users.noreply.github.com>
2024-08-23 11:43:18 -0600

    Inplementacion de metodos basicos de autenticacion usando laravel-sanctum y configuraciones en el cors para permitir el acceso de ciertos dominios.

---
+++ app/Http/Controllers/Api/Auth/AuthApiController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
+
+
+/**
+ * Class AuthController
+ * @package App\Http\Controllers\Api\Auth
+ * @group Auth
+ */
+class AuthApiController extends Controller
+{
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'usuario_dominio' => 'required',
+            'password' => 'required',
+        ]);
+
+        if (!Auth::attempt($request->only('usuario_dominio', 'password'))) {
+            return response()->json([
+                'message' => 'Credenciales incorrectas'
+            ], 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Token eliminado con éxito'
+        ]);
+    }
+
+
+}
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::group(['prefix' => 'auth'], function () {
+
+    Route::post('login', [\App\Http\Controllers\Api\Auth\AuthApiController::class, 'login'])->middleware('guest');
+
+    Route::post('logout', [\App\Http\Controllers\Api\Auth\AuthApiController::class, 'logout'])->middleware('auth:sanctum');
+
+});
+
