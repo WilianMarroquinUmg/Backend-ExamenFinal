1dd9d5195dcd054f037907e281cd7bee277de143
WilianMarroquin <githubwiliangit@gmail.com>
2024-10-28 21:22:30 -0600

    Mejoras en Proyecto

---
+++ app/Http/Controllers/AppBaseController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+
+
+use Illuminate\Support\Facades\Response;
+
+use App\Utils\ResponseUtil;
+
+/**
+ * @SWG\Swagger(
+ *   basePath="/api/v1",
+ *   @SWG\Info(
+ *     title="Laravel Generator APIs",
+ *     version="1.0.0",
+ *   )
+ * )
+ * This class should be parent class for other API controllers
+ * Class AppBaseController
+ */
+class AppBaseController extends Controller
+{
+
+
+    /**
+     * @param string $message
+     * @param mixed  $data
+     *
+     * @return array
+     */
+    public static function makeResponse($message, $data)
+    {
+        return [
+            'success' => true,
+//            'rows'    => count($data),
+            'data'    => $data,
+            'message' => $message,
+        ];
+    }
+
+    public function sendResponse($result, $message)
+    {
+
+        return Response::json(ResponseUtil::makeResponse($message, $result));
+
+    }
+
+    public function sendError($error, $code = 404)
+    {
+        return Response::json(ResponseUtil::makeError($error), $code);
+    }
+
+    public function sendSuccess($message)
+    {
+        return Response::json([
+            'success' => true,
+            'message' => $message
+        ], 200);
+    }
+}
